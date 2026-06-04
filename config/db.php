06d949da807336c454b658d18313d245c88d39cd
Roman Desktop <rome.rei@gmail.com>
2026-06-04 18:04:55 +0300

    .htaccess addition

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ $user     = "root";
 $password = "";
 $dbname   = "personal_budget";
 
-require_once __DIR__ . "/../sql/init_db.php";
+require_once dirname(__DIR__) . "/sql/init_db.php";
 
 // מתחבר ל-MySQL, יוצר DB / טבלאות / נתוני זרע אם חסרים
 $conn = init_database($host, $user, $password, $dbname);
